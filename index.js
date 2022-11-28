509100e20b57821b479745f6cff8f06928f35527
Genevieve Ferrari <genferrari@gmail.com>
2022-11-27 17:44:04 -0800

    adding generateREADME code

--- index.js
+++ index.js
@@ -15,66 +15,80 @@ const generateMD = () =>
     - [Tests](#tests)
     
     ## Installation 
+    ${installation}
 
     ## Usage
+    ${usage}
 
     ## License 
+    ${liscense}
 
     ## Contributing 
+    ${contributing}
 
     ## Tests 
+    ${tests}
 
     ## Questions
     To see more of my work, follow me at github.com/${username}.
     For further questions about this project, contact me at ${email}.
 `;
 
-inquirer.prompt([
-  {
-    type: "input",
-    name: "projectTitle",
-    message: "What is the name of your project?",
-  },
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "projectTitle",
+      message: "What is the name of your project?",
+    },
 
-  {
-    type: "input",
-    name: "username",
-    message: "What is your github username?",
-  },
+    {
+      type: "input",
+      name: "username",
+      message: "What is your github username?",
+    },
 
-  {
-    type: "input",
-    name: "email",
-    message: "What is your email address?",
-  },
+    {
+      type: "input",
+      name: "email",
+      message: "What is your email address?",
+    },
 
-  {
-    type: "input",
-    name: "description",
-    message:
-      "Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide: What was your motivation?  Why did you build this project? What problem does it solve?",
-  },
+    {
+      type: "input",
+      name: "description",
+      message:
+        "Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide: What was your motivation?  Why did you build this project? What problem does it solve?",
+    },
 
-  {
-    type: "list",
-    message: "What type of liscense would you like to use?",
-    name: "liscense",
-    choices: ["MIT", "GPLv3", "GPL"],
-  },
+    {
+      type: "list",
+      message: "What type of liscense would you like to use?",
+      name: "liscense",
+      choices: ["MIT", "GPLv3", "GPL"],
+    },
 
-  //   {
-  //     type: "checkbox",
-  //     message: "What languages do you know?",
-  //     name: "stack",
-  //     choices: ["HTML", "CSS", "JavaScript", "MySQL"],
-  //   },
-  //   {
-  //     type: "list",
-  //     message: "What is your preferred method of communication?",
-  //     name: "contact",
-  //     choices: ["email", "phone", "telekinesis"],
-  //   },
-]);
+    //   {
+    //     type: "checkbox",
+    //     message: "What languages do you know?",
+    //     name: "stack",
+    //     choices: ["HTML", "CSS", "JavaScript", "MySQL"],
+    //   },
+    //   {
+    //     type: "list",
+    //     message: "What is your preferred method of communication?",
+    //     name: "contact",
+    //     choices: ["email", "phone", "telekinesis"],
+    //   },
+  ])
+  .then((answers) => {
+    const readmeContent = generateMD(answers);
+
+    fs.writeFile("README.md", readmeContent, (err) =>
+      err ? console.log(err) : console.log("Successfully created Readme!")
+    );
+  });
+
 //   .then((data) => {
 //     const filename = `${data.name.toLowerCase().split(" ").join("")}.json`;
 
